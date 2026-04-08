7d85a37089703b4280909a7c090c72596dac60f4
htphong0909 <htphong159951@gmail.com>
2026-04-09 02:28:30 +0700

    feat: integrate Gemini RAG with NestJS and Python Model API (using database)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+<<<<<<< HEAD
+=======
+import { FeedModule } from './feed/feed.module';
+>>>>>>> a355465629c1e4ab8391fc9ba663ee44f6357241
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+<<<<<<< HEAD
+=======
+    FeedModule,
+>>>>>>> a355465629c1e4ab8391fc9ba663ee44f6357241
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
